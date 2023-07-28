ccbbb2f95746589e9f2c412946a2c6280a0e62cf
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2023-07-28 15:26:09 +0100

    initial app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import barbieData from "./barbie_photos.json";
 
 function App() {
+  const [randomBarbie, setRandomBarbie] = useState(null);
+
+  const handleStartButtonClick = () => {
+    const randomIndex = Math.floor(Math.random() * barbieData.length);
+    setRandomBarbie(barbieData[randomIndex]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          className="Barbie-logo"
+          src="/images/Barbie_Logo.svg.webp"
+          alt="Barbie Logo"
+        />
       </header>
+      <div className="App-line"></div>
+      <body className="App-body">
+        <h1> If You Were a Barbie Movie Character, Who Would You Be?</h1>
+        <div className="character-app-container">
+          {!randomBarbie ? ( // Display placeholder photo if randomBarbie is null
+            <div className="character-item">
+              <img
+                className="character-image"
+                src="/images/placeholder-barbie.jpg"
+                alt="placeholder photo of barbie with pink ponytale"
+              />
+              <div className="character-name"></div>
+            </div>
+          ) : (
+            // Display the selected randomBarbie if it's not null
+            <div className="character-item">
+              <img
+                className="character-image"
+                src={randomBarbie.link}
+                alt={randomBarbie.name}
+              />
+              <div className="character-name">{randomBarbie.name}</div>
+            </div>
+          )}
+        </div>
+        <button className="start-button" onClick={handleStartButtonClick}>
+          Start
+        </button>
+      </body>
     </div>
   );
 }
